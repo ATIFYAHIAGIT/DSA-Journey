0262ea58bc467aa0502f01f40ab064f66699e26f
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-05-24 20:31:35 +0530

    learnt to evalue power of a number

--- power_of_a_num.cpp
+++ power_of_a_num.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    cout<<"Enter number: ";
+    int n,m,i,num;
+    cin>>n;
+    cout<<"Enter the exponent of the number: ";
+    cin>>m;
+    
+    num=1;
+
+    if (m==0){
+        cout<<"1";
+    }
+
+    else if (m==1) {
+        cout<<"m";
+    }
+
+    else { 
+    for (i=2;i<=m+1;i++ ) {
+        num=num*n;
+    }
+        cout<<num;}
+    return 0;
+}
