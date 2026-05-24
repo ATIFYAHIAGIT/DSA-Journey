f1f829689a1bb1d0fc73c44fc42ef7c65ed15ea7
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-05-24 19:37:54 +0530

    for loop bhi seekh hi liya

--- for_loop.cpp
+++ for_loop.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    // to aaj ham print karenge counting 1 se 100 tak, ya nahi, khud hi number bhi le lete hain
+    cout<<"Enter a number upto which counting has to be printed: ";
+    int n,i;
+    cin>>n;
+
+    for (i=1;i<=n;i++) {
+        cout<<i<<"\n";
+
+    }
+return 0;
+    
+}
