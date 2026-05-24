233a976bb46c121156f5588d70214db120d2a716
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-05-24 23:12:08 +0530

    prime number

--- prime.cpp
+++ prime.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    cout<<"Enter a number: ";
+    int i,x;
+    cin>>x;
+
+    if (x<2) {
+        cout<<"x is not a prime number";
+    }
+
+    for (i=2;i<x;i++) {
+        if (x%i==0) {
+            cout<<"Not a prime number";
+            return 0;
+        }
+    }
+
+    cout<<"Prime number";
+    return 0;
+}
