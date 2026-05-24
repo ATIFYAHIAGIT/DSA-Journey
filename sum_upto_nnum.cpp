412f21438ecaf8254c23a16a9ee2c9f7fed8c809
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-05-24 20:43:50 +0530

    sum upto n terms using loops

--- sum_upto_nnum.cpp
+++ sum_upto_nnum.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    cout<<"Enter upto what number of terms do you what the sum: ";
+    int x,i,sum;
+    cin>>x;
+    sum=0;
+    for (i=1;i<=x;i++) {
+        sum=sum+i;
+    }
+    cout<<"The sum of n terms is: "<<sum;
+    return 0;
+}
