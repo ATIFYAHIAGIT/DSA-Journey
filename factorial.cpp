f9e0f07b2db043b91e2fb71632c4ef5607628bd8
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-05-24 22:52:33 +0530

    computing fatorial through for loop

--- factorial.cpp
+++ factorial.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout<<"Enter a number: ";
+    int i,x;
+    cin>>x;
+    int factorial=1;
+    for (i=1;i<=x;i++) {
+        factorial=factorial*i;
+    }
+    cout<<"The factorial of x is: "<<factorial;
+return 0;   
+}
+
 
