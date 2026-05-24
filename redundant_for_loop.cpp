fab6f05b32a4252a6da28d8bfe804ca05d492000
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-05-24 19:51:01 +0530

    same things repeat

--- redundant_for_loop.cpp
+++ redundant_for_loop.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int n,i;
+    cout<<"Enter number: ";
+    cin>>n;
+    for (i=1;i<=n;i=i+1) {
+        cout<<"Tera naam dhokha rakh du, naraaz hogi kya."<<"\n";
+    }
+    return 0;
+}
