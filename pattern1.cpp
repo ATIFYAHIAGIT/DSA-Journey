3eac87ef09b30df6d0b8cf2d7105dda0e75457b2
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-05-30 11:15:00 +0530

    1st pattern printed succesfully

--- pattern1.cpp
+++ pattern1.cpp
@@ -1,1 +1,17 @@
+#include <iostream> 
+using namespace std;
 
+int main () 
+{
+    cout<<"Enter the number of rowns and columns: ";
+    int x,i,j;
+    cin>>x;
+
+    for (i=1;i<=x;i++) {
+        for (j=1;j<=x;j++)  {
+            cout<<"*"<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
